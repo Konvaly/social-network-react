d243d4c7c9b1112cb66668d010e815c14bf37ef3
Mariia <konvaly@gmail.com>
2021-08-10 22:32:22 +0300

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div>
+        <a href="https://www.google.com/">Profile</a>
+      </div>
+      <div>
+        <a href="https://www.google.com/">Messages</a>
+      </div>
+      <div>
+        <a href="https://www.google.com/">News</a>
+      </div>
+      <div>
+        <a href="https://www.google.com/">Music</a>
+      </div>
+      <div>
+        <a href="https://www.google.com/">Settings</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbZTPBifRk4nLz9dLqgKsJxFIkt8HgzGfqNQ&usqp=CAU"
+          alt="beach"
+        ></img>
+        <div>ava + description</div>
+        <div>
+          My posts
+          <div>New post</div>
+          <div>
+            <div>post1</div>
+            <div>post2</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Profile;
